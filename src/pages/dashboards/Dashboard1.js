af3117278479abc3a2d19116d8379ebf241134bd
Tejaswini-Gurumoorthy <75904755+Tejaswini-Gurumoorthy@users.noreply.github.com>
2023-06-28 15:55:20 +0530

    changed city to country

--- src/pages/dashboards/Dashboard1.js
+++ src/pages/dashboards/Dashboard1.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TextBox from '../../components/TextBox'
 
 function Dashboard1() {
- 
+
   return (
     <div>
       <div className='dashboard-body'>
@@ -25,8 +25,8 @@ function Dashboard1() {
             <option value='TN'>Tamil Nadu</option>
           </select>
           <select className='text-box-plain' value='City'>
-            <option value='City'>City</option>
-            <option value='chennai'>Chennai</option>
+            <option value='Country'>Country</option>
+            <option value='india'>India</option>
           </select>
           <TextBox placeholder='Pincode' />
         </div>
